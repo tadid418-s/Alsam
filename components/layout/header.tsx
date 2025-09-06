e79d95220463be552d6a511df3c0e1b7608089f9
tadid418-s <tadiyos.dejene@aastustudent.edu.et>
2025-09-06 19:21:31 +0300

    nav bar update

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -15,20 +15,11 @@ import Navigation from './navigation'
 export interface HeaderProps extends Omit<BoxProps, 'children'> {}
 
 export const Header = (props: HeaderProps) => {
-  const ref = React.useRef<HTMLHeadingElement>(null)
-  const [y, setY] = React.useState(0)
-  const { height = 0 } = ref.current?.getBoundingClientRect() ?? {}
-
-  const { scrollY } = useScroll()
-  React.useEffect(() => {
-    return scrollY.on('change', () => setY(scrollY.get()))
-  }, [scrollY])
-
+  // Always use scrolled state
   const bg = useColorModeValue('whiteAlpha.700', 'rgba(29, 32, 37, 0.7)')
 
   return (
     <Box
-      ref={ref}
       as="header"
       top="0"
       w="full"
@@ -38,9 +29,9 @@ export const Header = (props: HeaderProps) => {
       borderColor="whiteAlpha.100"
       transitionProperty="common"
       transitionDuration="normal"
-      bg={y > height ? bg : ''}
-      boxShadow={y > height ? 'md' : ''}
-      borderBottomWidth={y > height ? '1px' : ''}
+      bg={bg}
+      boxShadow="md"
+      borderBottomWidth="1px"
       {...props}
     >
       <Container maxW="container.2xl" px="8" py="4">
--- data/logo.tsx
+++ data/logo.tsx
@@ -1,10 +1,18 @@
 import { Box, Image } from '@chakra-ui/react'
 import NextLink from 'next/link'
 
-export const Logo = (props) => (
-  <NextLink href="/" passHref legacyBehavior>
-    <Box as="a" display="flex" alignItems="center" sx={{ textDecoration: 'none', '&:hover': { textDecoration: 'none' } }} {...props}>
+export const Logo = ({ disableLink, ...props }) => {
+  const logoImg = (
+    <Box display="flex" alignItems="center" sx={{ textDecoration: 'none', '&:hover': { textDecoration: 'none' } }} {...props}>
       <Image src="/static/favicons/ALsam_Transparent.png" alt="AL-SAM logo" height="40px" width="auto" />
     </Box>
-  </NextLink>
-)
+  );
+  if (disableLink) return logoImg;
+  return (
+    <NextLink href="/" passHref legacyBehavior>
+      <Box as="a" display="flex" alignItems="center" sx={{ textDecoration: 'none', '&:hover': { textDecoration: 'none' } }}>
+        <Image src="/static/favicons/ALsam_Transparent.png" alt="AL-SAM logo" height="40px" width="auto" />
+      </Box>
+    </NextLink>
+  );
+}
